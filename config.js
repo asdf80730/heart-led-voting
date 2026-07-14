71f37aa8ac3aa74e6f1d09fd14f0cabc8a4fbef9
asdf80730 <asdf80730@gmail.com>
2026-07-14 18:57:38 +0800

    Update APP_CONFIG with new LIFF_ID and GAS_API_URL

---
+++ config.js
@@ -0,0 +1,4 @@
+window.APP_CONFIG = {
+  LIFF_ID: '2008484338-OYHvqZr9',
+  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbyh_hfMDNbI-7kw8wjrDjeYE2AqjPCTdhps5JfpSKKQYkkZtqtDGxeg7r3zxdaWMj02/exec'
+};
